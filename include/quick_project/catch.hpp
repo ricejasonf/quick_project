a8916b1483e9970a0a4271917ad54ed14f75f03f
Jason Rice <ricejasonf@gmail.com>
2018-03-14 13:26:00 -0700

    Adds catch test stuffs

---
+++ include/quick_project/catch.hpp
@@ -0,0 +1,45 @@
+//
+// Copyright (c) 2012 Two Blue Cubes Ltd. All rights reserved.
+// Copyright Jason Rice 2016
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+#ifndef QUICK_PROJECT_TEST_CATCH_HPP
+#define QUICK_PROJECT_TEST_CATCH_HPP
+
+#ifdef QUICK_PROJECT_USE_CATCH
+#include <quick_project/catch_orig.hpp>
+#else
+
+#include <cstdlib>
+#include <vector>
+
+// Custom shim for Catch Test Framework
+
+extern std::vector<void (*)()> catch_shim_test_cases;
+
+struct catch_shim_test_case
+{
+  template <typename Fn>
+  catch_shim_test_case(Fn fn)
+  {
+    catch_shim_test_cases.push_back(fn);
+  }
+};
+
+#define INTERNAL_CATCH_UNIQUE_NAME_LINE2( name, line ) name##line
+#define INTERNAL_CATCH_UNIQUE_NAME_LINE( name, line ) INTERNAL_CATCH_UNIQUE_NAME_LINE2( name, line )
+#define INTERNAL_CATCH_UNIQUE_NAME( name ) INTERNAL_CATCH_UNIQUE_NAME_LINE( name, __LINE__ )
+
+#define TEST_CASE( ... ) \
+    static void INTERNAL_CATCH_UNIQUE_NAME(CATCH_SHIM_TEST)(); \
+    namespace{ catch_shim_test_case INTERNAL_CATCH_UNIQUE_NAME( autoRegistrar )( &INTERNAL_CATCH_UNIQUE_NAME(CATCH_SHIM_TEST)); }\
+    static void INTERNAL_CATCH_UNIQUE_NAME(CATCH_SHIM_TEST)()
+
+#define REQUIRE(cond) if (!(cond)) std::exit(1);
+#define CHECK(cond)   if (!(cond)) std::exit(1);
+
+#endif
+
+#endif
